15a4063f885978e6f6f1f6289f5e1841144b4d96
Krish Patel <krish.sojitra141224@marwadiuniversity.ac.in>
2026-09-10 17:25:13 +0530

    Add files via upload

---
+++ WebForm2.aspx
@@ -0,0 +1,77 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm2.aspx.cs" Inherits="Practical_5.WebForm2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Leave Application</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            Leave Application
+            <br />
+            <br />
+
+            Employee Name:
+            <asp:TextBox ID="TextBox3" runat="server" OnTextChanged="TextBox3_TextChanged"></asp:TextBox>
+
+            <br />
+            <br />
+
+            Leave Date:
+            
+
+            <asp:Label ID="lblleaveDate" runat="server"></asp:Label>
+
+            <br />
+
+            LeaveType:
+
+            <asp:DropDownList ID="DropDownList1" runat="server"
+                OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+
+                <asp:ListItem>Personal Leave</asp:ListItem>
+                <asp:ListItem>Medical Leave</asp:ListItem>
+                <asp:ListItem>Emergency leave</asp:ListItem>
+
+            </asp:DropDownList>
+
+            <br />
+            <br />
+
+            Reason:
+
+            <asp:TextBox ID="TextBox2" runat="server"
+                Height="65px" Width="166px"></asp:TextBox>
+
+            <br />
+            <br />
+
+            Remember Name:
+
+            <asp:CheckBox ID="CheckBox1" runat="server"
+                OnCheckedChanged="CheckBox1_CheckedChanged"
+                Text="Remember my name" />
+
+            <br />
+            <br />
+            <br />
+
+            <asp:Button ID="Btnsubmit" runat="server"
+                OnClick="Btnsubmit_Click"
+                Text="Submit Leave" />
+
+            <br />
+            <br />
+
+        </div>
+
+        <p>
+            <asp:Label ID="lblMsg" runat="server"></asp:Label>
+        </p>
+
+    </form>
+</body>
+</html>
---
+++ WebForm2.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Practical_5
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check whether Employee Name is available in cookies
+                if (Request.Cookies["EmpName"] != null)
+                {
+                    TextBox3.Text = Request.Cookies["EmpName"].Value;
+                }
+
+                // Get selected Leave Date from Session
+                if (Session["leaveDate"] != null)
+                {
+                    DateTime lvDate = (DateTime)Session["leaveDate"];
+
+                    lblleaveDate.Text = lvDate.ToString("dd-MM-yyyy");
+                }
+                else
+                {
+                    lblleaveDate.Text = "No Date Selected.......";
+                }
+            }
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Leave Type selection
+        }
+
+        protected void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            // Remember Employee Name using Cookie
+
+            if (CheckBox1.Checked)
+            {
+                Response.Cookies["EmpName"].Value = TextBox3.Text;
+                Response.Cookies["EmpName"].Expires =
+                    DateTime.Now.AddDays(7);
+            }
+            else
+            {
+                Response.Cookies["EmpName"].Expires =
+                    DateTime.Now.AddDays(-1);
+            }
+        }
+
+        protected void Btnsubmit_Click(object sender, EventArgs e)
+        {
+            // Submit Leave button
+
+            string empnm = TextBox3.Text;
+            string leavetype = DropDownList1.SelectedValue;
+            string reason = TextBox2.Text;
+
+            // Store Employee Name, Leave Type and Reason in Session
+            Session["Empnm"] = empnm;
+            Session["leaveType"] = leavetype;
+            Session["Reason"] = reason;
+
+            // Create Cookie if Checkbox is Selected
+            if (CheckBox1.Checked)
+            {
+                Response.Cookies["EmpName"].Value = empnm;
+                Response.Cookies["EmpName"].Expires =
+                    DateTime.Now.AddDays(7);
+            }
+
+            lblMsg.Text = "<b>Leave Submitted Successfully.......</b><br/>" +
+                "Employee Name: " + empnm + "<br/>" +
+                "Leave Type: " + leavetype + "<br/>" +
+                "Reason: " + reason;
+        }
+
+        protected void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
